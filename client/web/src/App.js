e2f869461fe0063ff0deb110c11122a4ecf21aa2
JuanPablo-Llorente <juanpablollorentej@gmail.com>
2024-02-24 19:06:25 -0300

    react web client setup

---
+++ client/web/src/App.js
@@ -0,0 +1,18 @@
+// Dependencies
+import React from "react";
+// Files
+// import Home from "./Home/Home";
+import "./App.css";
+
+
+function App()
+{
+    return (
+        <React.Fragment>
+            {/* <Home/> */}
+        </React.Fragment>
+    );
+};
+
+
+export default App;
---
+++ client/web/src/index.js
@@ -0,0 +1,11 @@
+// Dependencies
+import React from "react";
+import ReactDOM from "react-dom";
+// Files
+import App from "./App";
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
